a4b56d3c65783188274414ca7cee20d0e64acdb9
kotnid <tkt0506tkt@gmail.com>
2021-07-07 15:12:19 +0800

    bug fixed

--- client.py
+++ client.py
@@ -4,8 +4,10 @@ import asyncio
 
 async def hello(uri):
     async with websockets.connect(uri) as websocket:
-        await websocket.send("tkt")
+        ans = input("What is you name:")
+        await websocket.send(ans)
         name = await websocket.recv()
         print(f"recv : {name}")
 
-asyncio.run(hello("ws:/localhost:8964"))        
+asyncio.get_event_loop().run_until_complete(
+    hello('ws://localhost:8964'))
--- server.py
+++ server.py
@@ -3,7 +3,10 @@ import websockets
 
 async def echo(websocket , path):
     async for msg in websocket:
+        print(msg , 'receive from client')
         greeting = f"Hi {msg}"
         await websocket.send(greeting)
     
-asyncio.run(websockets.serve(echo , 'localhost' , 8964))
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 8964))
+asyncio.get_event_loop().run_forever()
