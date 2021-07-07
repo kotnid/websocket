53699179392314e414aed137411f009eb518acd0
kotnid <tkt0506tkt@gmail.com>
2021-07-07 16:20:10 +0800

    new

---
+++ script.js
@@ -0,0 +1,26 @@
+var minus = document.querySelector('.minus'),
+    plus = document.querySelector('.plus'),
+    value = document.querySelector('.value'),
+    users = document.querySelector('.users'),
+    websocket = new WebSocket("ws://127.0.0.1:8964/");
+
+minus.onclick = function(event){
+    websocket.send(JSON.stringify({action:'minus'}));
+}
+plus.onclick = function(event){
+    websocket.send(JSON.stringify({action:'plus'}));
+}
+websocket.onmessage = function(event){
+    data = JSON.parse(event.data);
+    switch (data.type){
+        case 'state':
+            value.textContent = data.value;
+            break;
+        case 'users':
+            value.textContent = (
+                data.count.toString() + "user" + (data.count == 1 ?"":"s"));
+                break;
+        default:
+            console.error("unsupported event" , data)            
+    }
+}
